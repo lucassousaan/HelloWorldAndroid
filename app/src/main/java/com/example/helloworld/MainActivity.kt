a878ae7e19971b0ebb426a59ccc74fecdd100ab4
lucassousaan <lucas11.ptc@gmail.com>
2022-03-26 13:24:56 -0300

    first commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView: TextView? = findViewById(R.id.textHello);
+
+        textView?.text = "Trabalho de ASI"
+    }
+}
